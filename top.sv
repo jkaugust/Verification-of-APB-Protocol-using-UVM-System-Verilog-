7ec6fd7b2bf4ac8530a4d3924e1c8b9a74ed4c1a
Justin Kurumullamkattil Augustine <31999616+jkaugust@users.noreply.github.com>
2018-09-27 01:26:59 -0400

    Add files via upload

--- top.sv
+++ top.sv
@@ -1,4 +1,4 @@
-module top (clk,rst,addr_in,wr_in,sel,
+module top (clk,rst,addr_in,wr_in,sel,data_in,
   wr_out1,addr_out1,data_out1,wr_out2,addr_out2,data_out2,
   wr_out3,addr_out3,data_out3,wr_out4,addr_out4,data_out4);
   input clk;
@@ -6,6 +6,7 @@ module top (clk,rst,addr_in,wr_in,sel,
   input [7:0] addr_in;
   input wr_in; //write when 1 and read when 0
   input sel;
+  input [31:0] data_in;
   output wr_out1,wr_out2,wr_out3,wr_out4;
   output [7:0] addr_out1,addr_out2,addr_out3,addr_out4;
   output [31:0] data_out1,data_out2,data_out3,data_out4;
